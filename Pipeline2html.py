c3d9115c6ef283190c4daab9808c0ad3b0b172c7
奥力 <S1n0xt0nHK@protonmail.com>
2021-05-11 22:17:22 -0300

    Update Pipeline2html.py
    A proposta que adicionei foram criação de menu e tratativas de erros.
    Os próximos commits gostaria de evoluir os tipos de scan e reports/layouts

--- Pipeline2html.py
+++ Pipeline2html.py
@@ -3,10 +3,19 @@ import json
 import os
 from json2html import *
 import re
+import argparse as ap
+
 jsonfile = 'results.json'
    
-def pipelinescan():
-    os.system('java -jar pipeline-scan.jar --veracode_api_id "api id" --veracode_api_key "api key" --file "application.zip" --issue_details true  --json_output true --filtered_json_output_file filtered.json')
+def pipelinescan(application, veracodeId, veracodeKey, scan_tipo):
+    try:
+        if (application == None) | (veracodeId == None) | (veracodeKey == None):
+            print("[-] Informar o parametros!")
+        else:     
+            print(f'[+] PipelineScan do tipo {scan_tipo}, executado no produto {application}')
+            os.system('java -jar pipeline-scan.jar --veracode_api_id str(veracodeId) --veracode_api_key str(strveracodeKey) --file application --issue_details true --json_output true --filtered_json_output_file filtered.json')
+    except:
+        raise Exception('Incorrect scan format')
 
 def getHTML():
     with open(jsonfile) as json_file:
@@ -16,6 +25,7 @@ def getHTML():
         clean = re.sub('<[^<]+?>', '', data)
         clean = clean.replace('\n', '')
         html = json2html.convert(json=clean, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
+        
         html2 = """
         <html>
         <head>
@@ -28,11 +38,31 @@ def getHTML():
         </body>
         </html>
         """
+
         f = open("pipeline-report.html", "w")
         f.write(html2)
         f.close()
 
-pipelinescan()		
+def gerarReport():
+   pass
+	
+if __name__ == '__main__':
+    parser = ap.ArgumentParser(description='Rodar o programa dessa forma: python3 Pipeline2html.py --aplicacao iFood --veracodeId {ID} --veracodeKey {KEY} -scan_tipo 1')
+    parser.add_argument('--aplicacao', help='Aplicacao para analise de seguranca em formato .zip. Ex. (TomatoApp.zip)', required=True)
+    parser.add_argument('--veracodeId', help='Informar o VeraCode ID', required=True)
+    parser.add_argument('--veracodeKey', help='Informar o VeraCode Key', required=True)  
+    parser.add_argument('--scan_tipo',help=' [1] SAST, [2] DAST, [3] SCA ', required=True)
 
-getHTML()
+    args = parser.parse_args()
 
+    # Load PipelineScan
+    aplicacao = args.aplicacao
+    veracodeId = args.veracodeId
+    veracodeKey = args.veracodeKey
+    scan_tipo = args.scan_tipo
+    
+    pipelinescan(aplicacao, veracodeId, veracodeKey, scan_tipo)
+    
+    #
+    # Generate HTML
+    getHTML()
