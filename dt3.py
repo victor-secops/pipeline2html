5cc7a2922b7c0fff4042d23d6a35fee35ace1bd0
Victor <victordevsecops@gmail.com>
2021-05-13 10:23:59 -0300

    upload
    adicionado maneira de autenticação usando Veracode credentials file

---
+++ dt3.py
@@ -0,0 +1,38 @@
+import sys
+import json
+import os
+from json2html import *
+import re
+jsonfile = 'results.json'
+   
+def pipelinescan():
+    os.system('java -jar pipeline-scan.jar --file nodegoat.zip --issue_details true')
+
+def getHTML():
+    with open(jsonfile) as json_file:
+        pipelinedata = json.load(json_file)
+        data = json.dumps(pipelinedata, indent=4)
+        
+        #clean = re.sub('<[^<]+?>', '', data)
+        #clean = clean.replace('\n', '')
+        html = json2html.convert(json=data, encode=on, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
+        html2 = """
+        <html>
+        <head>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        </head>
+        <body>
+        """+html+"""
+        </body>
+        </html>
+        """
+        f = open("pipeline-report.html", "w")
+        f.write(html2)
+        f.close()
+
+#pipelinescan()		
+
+getHTML()
+
